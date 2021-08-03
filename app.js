0958612cd8381ed1ac443c6e6ee762ddf46fe5c1
berkmahim <56481068+berkmahim@users.noreply.github.com>
2021-08-03 14:11:03 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelector(".card-body")[0];
+const secondCardBody = document.querySelector(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners();
+
+function eventListeners(){
+
+    form.addEventListener("submit",addTodo);
+}
+function addTodo(e){
+    const newTodo = todoInput.value;
+    console.log(newTodo);
+
+    e.preventDefault();
+}
