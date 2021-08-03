13731403b7a275bdb34f9df199d8b72444ca8b4f
berkmahim <56481068+berkmahim@users.noreply.github.com>
2021-08-03 18:06:06 +0300

    add todo

--- app.js
+++ app.js
@@ -13,8 +13,22 @@ function eventListeners(){
     form.addEventListener("submit",addTodo);
 }
 function addTodo(e){
-    const newTodo = todoInput.value;
+    const newTodo = todoInput.value.trim();
     console.log(newTodo);
-
+    addTodoToUI(newTodo);
     e.preventDefault();
 }
+function addTodoToUI(newTodo){
+
+}
+
+const listItem = document.createElement("li");
+const link = document.createElement("a");
+link.href = "#";
+link.className = "delete-item";
+link.innerHTML =  "<i class = 'fa fa-remove'></i>";
+listItem.className = "list-group-item d-flex justify-content-between";
+listItem.appendChild(document.createTextNode(newTodo));
+listItem.appendChild(link);
+
+todoList.appendChild(listItem);
